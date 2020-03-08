7fcfa95f9c351659271b8a5bf83f47a07febdf04
Ravi Sankar Karuturi <ravisankarkaruturi@gmail.com>
2020-03-07 17:21:08 -0800

    Bit Operation practice

--- src/main/java/com/prepare/algo/practice/BitOperations.java
+++ src/main/java/com/prepare/algo/practice/BitOperations.java
@@ -9,7 +9,9 @@ public class BitOperations {
     public static void main(String[] args){
 
         int x =50;
-
+        System.out.println(Integer.toBinaryString(10));
+        System.out.println(Integer.toBinaryString(-11));
+        System.out.println(1L<<5 | 1L<<2);
         System.out.println(6&4); // (0110 & 0100 = 0100)
         System.out.println(1|2); // (0001 | 0010)
         System.out.println(8>>1);// 1000>> 1 = 0100(Signed right shift)
@@ -24,10 +26,14 @@ public class BitOperations {
             System.out.println(i+":"+countOneBits(i));
         }
         System.out.println(1001&0);
-        char[] ch = new char[]{'a','b'};
+        parity(50);
+        fastparity(50);
+        System.out.println(0-1);
+        swapBits(32, 5,3);
 
     }
 
+    //Big O of (n); n is no of bits in x
     public static int countOneBits(int x){
         short count =0;
         while(x!=0){
@@ -40,16 +46,50 @@ public class BitOperations {
     /**
      * parity of a word is 1 when no:of 1's in binary is odd
      *         or else its 0
+     * //Big O of (n); n is no of bits in word
      * @param word (64 bit long word)
      * @return  1 or 0
      */
     public static short parity(long word){
         short result = 0;
+        int i=0;
         while(word !=0){
+            System.out.println(++i);
             result ^= (word & 1);
            word >>>= 1 ;
         }
         return result;
+    }
+
+    //Big O of (k); k is no of 1's in word
+    public static short fastparity(long word){
+        short result = 0;
+        int i=0;
+        while(word !=0){
+            System.out.println(++i);
+            result ^= 1;
+            word &= (word-1); // Drop lowest set bit of xs
+        }
+        return result;
+    }
+
+    /**
+     * swap ith and jth bit in the given x and viceversa
+     * @param x
+     * @param i
+     * @param j
+     */
+    public static void swapBits(long x , int i, int j){
+        long y =x;
+        if((x >>> i & 1) != (x >>> j & 1)){
+            // ith and jth bit are different so we need to flip the values.
+            // Select the bits to flip using bitmask
+            // if x = 1  then x^1 = 0
+            //    x = 0  then x^1 = 1
+            long bitmask = (1L << i) | (1L << j);
+            x ^= bitmask;
+        }
+        System.out.println("After swapping "+i+ "and "+j+" bits of "+y+" is::"+x);
     }
 
 
